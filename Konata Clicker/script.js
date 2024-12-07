e8b8015c1145fd2a42bc938dc8c87c370a223169
Shawn Huang <69904687+Mutton9558@users.noreply.github.com>
2024-12-07 16:46:46 +0800

    Random images appearing upon upgrade and volume slider

--- Konata Clicker/script.js
+++ Konata Clicker/script.js
@@ -2,13 +2,17 @@ const SCORETEXT = document.querySelector("#score");
 const TARGET = document.querySelector("#target-img");
 const UPGRADETEXT = document.querySelector("#upgrade-amount");
 const UPGRADEBTN = document.querySelector("#upgrade-btn");
-const AUDIO1 = document.querySelector("#audio1");
-const AUDIO2 = document.querySelector("#audio2");
+let volumeBg = document.querySelector('#volume-slider');
+let volumeClick = document.querySelector("#volume-slider-click");
+let randImageLoc = document.querySelector("#random-konata-img");
 
-var clickAudio = new Audio("momoi_nword.mp3");
+var clickAudio = new Audio("konata_good_job.mp3");
+var AUDIO1 = new Audio("./music/bgmusic1.mp3");
+var AUDIO2 = new Audio("./music/bgmusic2.mp3");
 var clickStrength = 1;
 var cur_score = 0;
 var upgrade_amount = 10;
+var imgID = 0;
 
 TARGET.addEventListener("click", ()=>{
     if (AUDIO1.paused && AUDIO2.paused) {
@@ -18,6 +22,15 @@ TARGET.addEventListener("click", ()=>{
     cur_score += clickStrength;
     SCORETEXT.innerHTML = cur_score;
     checkUpgradability();
+})
+
+volumeBg.addEventListener("change", function(e) {
+    AUDIO1.volume = e.currentTarget.value / 100;
+    AUDIO2.volume = e.currentTarget.value/100;
+})
+
+volumeClick.addEventListener("change", function(e) {
+    clickAudio.volume = e.currentTarget.value / 100;
 })
 
 function playBackgroundAudio(){
@@ -58,6 +71,10 @@ function upgradeClicks(){
     UPGRADETEXT.innerHTML = upgrade_amount;
     UPGRADEBTN.disabled = true;
     UPGRADEBTN.classList.add("btn-disabled")
+    topLocation = Math.floor(Math.random()*96);
+    leftLocation = Math.floor(Math.random()*96);
+    imgID += 1
+    randImageLoc.innerHTML += `<img src="img/konatagoodjob.png" style="position: absolute;top: ${topLocation}%;left: ${leftLocation}%;z-index: 1;" height="50" width="50" id="konata-random-img-${imgID}">`
 }
 
 function playAudio(){
